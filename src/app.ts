e2fbadc10fd182f2be9b53e919a40821961dc69e
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-06-23 15:38:32 +0600

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,15 @@
 import express from 'express';
-import prisma from './prisma';
 import cors from 'cors';
 import { corseOptions, morganMiddleware } from './middlewares/middleware';
-
+import userRouter from './routes/user.route';
 const app = express();
 app.use(express.json());
 
-// Middleware
+// Middlewares
 app.use(cors(corseOptions));
 app.use(morganMiddleware);
 
-app.get('/users', async (_req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-app.post('/users', async (req, res) => {
-  const { email, username, password } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: { email, username, password },
-    });
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ error: 'User creation failed', details: error });
-  }
-});
+// Route Middlewares
+app.use('/api/users', userRouter);
 
 export default app;

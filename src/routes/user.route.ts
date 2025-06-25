f151e6ef9ce344cbb4bcdb633a03d8cd91286f1d
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-06-26 00:17:54 +0600

    feat(routes): update user route to correctly handle user updates by changing POST method to updateUser

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,13 +6,16 @@ import {
   getOneUser,
   createUser,
   loginUser,
+  updateUser,
 } from '../controllers/user.controller';
 
 router.get('/all', getAllUsers);
 
+router.post('/', createUser);
+
 router.get('/:id', getOneUser);
 
-router.post('/', createUser);
+router.post('/:id', updateUser);
 
 router.post('/login', loginUser);
 

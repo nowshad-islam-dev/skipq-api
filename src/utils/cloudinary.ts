25c1d8884257c67458959b94c9f6da8787ffaa19
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-06-30 00:26:58 +0600

    refactor(cloudinary): enhance uploadToCloudinary function to support multiple folders

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -1,9 +1,14 @@
 import cloudinary from '../configs/cloudinary';
 
-const uploadToCloudinary = async (buffer: Buffer) => {
+// For uploading profile picture of an user and -->
+// Uploading multiple photos of a service
+
+type Folder = 'profile_pictures' | 'services_pictures';
+
+const uploadToCloudinary = async (buffer: Buffer, folder: Folder) => {
   return await new Promise((resolve, reject) => {
     const stream = cloudinary.uploader.upload_stream(
-      { folder: 'profile_pictures' },
+      { folder },
       (error, result) => {
         if (result) resolve(result);
         else reject(error);

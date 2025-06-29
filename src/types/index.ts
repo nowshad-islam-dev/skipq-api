e5ffd414a82f00d844a95ac2eded25774b473d76
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-06-30 00:26:46 +0600

    refactor(types): clean up user entry schemas and add service entry types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,7 @@
 import * as z from 'zod/v4';
 
+/* User related types */
 // Schema for a full user entry as stored in the database
-export const newUserEntryToDb = z.object({
-  id: z.number(),
-  email: z.email(),
-  phone: z.string(),
-  username: z.string().min(3),
-  hashedPassword: z.string(),
-});
 
 // Schema for user input when registering (no id, no hashedPassword yet).
 export const newUserEntry = z.object({
@@ -22,29 +16,43 @@ export const publicUserSchema = z.object({
   email: z.email(),
   username: z.string().min(3),
   phone: z.string(),
+  profilePicture: z.string().optional().nullable(),
   createdAt: z.date(),
 });
 
-export const updatingUserEntry = z
-  .object({
-    email: z.email().optional(),
-    phone: z.string().optional(),
-    newPassword: z.string().min(6).optional(),
-    password: z.string().min(6),
-  })
-  .refine(
-    (data) =>
-      data.email !== undefined ||
-      data.phone !== undefined ||
-      data.newPassword !== undefined,
-
-    {
-      error: 'At least one field must be provided to update',
-    },
-  );
+export const updatingUserEntry = z.object({
+  email: z.email().optional(),
+  phone: z.string().optional(),
+  newPassword: z.string().min(6).optional(),
+  password: z.string().min(6),
+});
 
 // Inferred TypeScript types from Zod schemas
-export type UserFromDb = z.infer<typeof newUserEntryToDb>;
+
 export type NewUserEntry = z.infer<typeof newUserEntry>;
 export type PublicUser = z.infer<typeof publicUserSchema>;
 export type UpdatingUser = z.infer<typeof updatingUserEntry>;
+
+/* Service related types */
+
+export const newServiceEntry = z.object({
+  serviceName: z.string().min(3),
+  serviceDescription: z.string().min(6),
+  averageWaitingTime: z.string().min(0),
+  serviceLocation: z.string().min(3),
+  userId: z.string(),
+});
+
+export const publicServiceSchema = z.object({
+  id: z.number(),
+  serviceName: z.string().min(3),
+  serviceDescription: z.string().min(6),
+  averageWaitingTime: z.string().min(0),
+  serviceLocation: z.string().min(1),
+  photos: z.array(z.string()).optional().nullable(),
+  userId: z.number(),
+  createdAt: z.date(),
+});
+
+export type NewServiceEntry = z.infer<typeof newServiceEntry>;
+export type PublicService = z.infer<typeof publicServiceSchema>;
